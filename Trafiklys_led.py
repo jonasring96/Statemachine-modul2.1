5a16f0c088a394f02f42d44a3c93e3fc382ae850
lkaae2018 <lak@djhhadsten.dk>
2018-11-17 13:33:17 +0100

    statemachine-modul2.1

--- Trafiklys_led.py
+++ Trafiklys_led.py
@@ -2,28 +2,28 @@ from random import random
 from gpiozero import LED
 from time import sleep
 
-led1=LED(12)
-led2=LED(13)
-led3=LED(23)
+led1=LED(13)
+led2=LED(12)
+led3=LED(5)
 
 def state0():
     print ("Grøn! Kør")
-    led1.on()
-    sleep(5.5)
     led1.off()
+    sleep(5.5)
+    led1.on()
     return state1
 
 def state1():
     print ("Gul! stop")
     led2.on()
-    sleep(1.5)
+    sleep(6.5)
     led2.off()
     return state2
 
 def state2():
     print ("Rød! stop")
     led3.on()
-    sleep(5.5)
+    sleep(6.5)
     led3.off()
     return state0
 
